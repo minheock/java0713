f3b0b54c1103df9d3a49b1bd8f102f963a3a6854
Phantom <ksj0633@naver.com>
2023-07-14 14:08:26 +0900

    과제 풀기 중간과정

--- src/ch03_system/SystemMain.java
+++ src/ch03_system/SystemMain.java
@@ -1,5 +1,7 @@
 package ch03_system;
 
+import java.util.Scanner;
+
 public class SystemMain {
 
 	public static void main(String[] args) {
@@ -21,7 +23,33 @@ public class SystemMain {
 		System.out.println("월\t화\t수\t목\t금");
 		// 일부 특수 문자를 출력하고 싶을 때 
 		System.out.println("오늘은 \" 금요일\" ^^");
+		// 역슬래시가 2개이상 중복되면 하나를 제외하고 문자열로 출력됨 
 		
+		// printf 포멧 문자열 형태로 내용을 출력
+		System.out.printf("원주율은 %.9f \n",3.141592654);
+		System.out.printf("%d명이 수업을 듣고 있습니다. \n" , 23);
+		System.out.printf("%d명이 %s를 듣고 있습니다. \n", 24, "수업");
+		
+		System.out.println("+++++++++표준입력++++++++++");
+		Scanner scan = new Scanner(System.in); // 입력이 들어올때까지 대기 
+		System.out.println("이름을 입력해주세요.");
+		System.out.println(">>>");
+		// 키보드 입력 내용 가져오기
+		String name= scan.nextLine();
+		System.out.println(name + "님 환영합니다. ^^");
+		
+		System.out.println("에어컨 희망 온도를 입력해 주세요.");
+		System.out.println(">>>");
+		int temper = Integer.parseInt(scan.nextLine());
+		System.out.printf("희망온도 %d 도 이군요.\n", temper);
+		
+		
+		
+		System.out.println("취미를 입력해 주세요.");
+		System.out.println(">>>");
+		String x = scan.nextLine();
+		System.out.printf("취미가 %s 이군요." , x);
+		scan.close();
 	}
 
 }
---
+++ src/practice/submit01/result01.java
@@ -0,0 +1,39 @@
+package practice.submit01;
+
+public class result01 {
+
+	public static void main(String[] args) {
+		/*1번
+		 * 본인의 이름과 나이, 키, 연락처, 이메일을 각각 타입에 맞는 변수에 저장한 후 아래와 같이 콘솔창에 출력해주세요.
+		 * 
+		 * 변수명: name, age, height, phone, email 사용 키는 소수 첫번째자리를 포함
+		 */
+		String name = "minheock";
+		int age = 30;
+		double height = 173.8;
+		String phone = "010-1234-5678";
+		String email = "dygks0602@naver.com";
+		System.out.println("이름:"+name +"\n나이:"+age + "\n키:" + height + "\n연락처:" + phone + "\n이메일:" + email);
+		
+		/* 2번
+		 * 문자열 변수 String enigma = “너오구늘리뭐너먹구지리”; 에서 너, 구, 리를 제거해서 출력해보세요.
+		 */
+		String enigma = "너오구늘리뭐너먹구지리";
+//		enigma = enigma.replaceAll("[너, 구, 리]", "");
+		enigma = enigma.replace("너", "");
+		System.out.println("1차 해독완료 [너]" + enigma );
+		enigma = enigma.replace("구", "");
+		System.out.println("2차 해독완료 [구]" + enigma);
+		enigma = enigma.replace("리", "");
+		System.out.println("3차 해독완료 [리]" + enigma);
+		System.out.println("해독 끝" + enigma);
+		
+		//3번
+		int example = 278;
+		int first = example % 2;
+		int mid = first % 10;
+		int last = example % 1;
+		System.out.println(mid);
+	}
+
+}
