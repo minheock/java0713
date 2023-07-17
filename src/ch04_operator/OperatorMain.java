27a422e86ae02669a0ec39538cb0cb280841aa00
Phantom <ksj0633@naver.com>
2023-07-17 09:50:25 +0900

    대입 연산자 연습

--- src/ch04_operator/OperatorMain.java
+++ src/ch04_operator/OperatorMain.java
@@ -19,6 +19,19 @@ public class OperatorMain {
 		a= num++;
 		b = ++ num;
 		System.out.printf("a : %d 는 할당 후 증가, b : %d 는 증가 후 할당.", a, b);
+		System.out.println("\n ========== 대입 연산자 =============/n");
+		num = 10;
+		num += 1;
+		System.out.println(num); //11
+		num -= 2;
+		System.out.println(num);//9
+		num *= 2;
+		System.out.println(num);//18
+		num /= 2; // 나누기의 몫 리턴.
+		System.out.println(num);//9
+		num %=2; // 나누기의 나머지 리턴.
+		System.out.println(num);
+
 	}
 
 }
