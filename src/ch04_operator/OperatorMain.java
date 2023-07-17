ec8388b6085f38d45ca208c68d111be8463bcfbc
Phantom <ksj0633@naver.com>
2023-07-17 10:42:14 +0900

    비교 연산자, 삼항 연산자 연습

--- src/ch04_operator/OperatorMain.java
+++ src/ch04_operator/OperatorMain.java
@@ -44,6 +44,69 @@ public class OperatorMain {
 		System.out.println("곱하기 :" + (numA * numB));
 		System.out.println("나누기 :"+ (numA / numB));
 		System.out.println("나머지 :"+ (numA % numB));
+		System.out.println("\n ========== 비교 연산자 =============/n");
+		// 비교 결과에 따라 true/ false가 리턴
+		int numFive = 5;
+		double pi = 3.14;
+		int numOne = 1;
+		System.out.println(numFive > numOne);
+		boolean result = numFive < numOne;
+		System.out.println(result);
+		// 타입이 다른 숫자도 비교가능
+		System.out.println(numFive >= pi);
+		
+		// == 같다 != 같지 않다
+		System.out.println(numFive == pi);		
+		System.out.println(numFive == 5);
+		
+		System.out.println("가 > 나");
+		System.out.println("가 < 나");
+		
+		String stuA= "김길동";
+		String stuB= "홍길동";
+		System.out.println("김 > 홍");
+		System.out.println(stuA.charAt(1));
+		System.out.println(stuA.charAt(0) > stuB.charAt(0));
+		
+		// 문자열이 서로 같은지 비교 .equals사용
+		 String ship = "배"; // 문자열 리터럴 사용 string 생성 (constant pool 이라는 공간에 저장)
+		 String pear = "배"; 
+		 String bae = new String("배"); // new 생성자 사용하여 객체 생성시 heap 영역에 저장
+		 System.out.println(ship == pear); // true
+		 System.out.println(ship == bae); // false 저장공간이 다름
+		 System.out.println(ship.equals(bae));//true 문자열 자체 비교
+		
+		 String serverDataString = null;
+		 System.out.println(serverDataString != null);
+		 
+		 // 주소 존재함 데이터가 없음
+		 String inputData = "";
+		 System.out.println(inputData == null);
+		 System.out.println(inputData.equals(""));
+		 System.out.println(inputData.length() == 0);
+		 System.out.println(inputData.isEmpty());
+		 
+		 System.out.println("\n ==================삼항 연산자================\n");
+		 String inputId = "nick1234";
+		 // inputId의 길이가 7 이상이면 통과, 아니면 재입력 출력
+		 // (a) ? (b) : (c) a의 식이 true 이면 b false 이면 c를 리턴.
+		 String check = (inputId.length() >= 7) ? ("통과") : ("재입력");
+		 System.out.println(check);
+		 
+		 
+		 // 이중 삼항 연산자
+		 // score 가 90보다 크면 grade "A" 80점 보다 크면 "B", 나머지는 C
+		 int score = 76;
+		 String grade = (score > 90) ? ("A"): ((score > 80) ? ("B") : ("C"));
+		 System.out.println(grade);
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
 	}
 
 }
