ad216c280394bd397f52b816bd0f268f255f64d1
Phantom <ksj0633@naver.com>
2023-07-25 14:02:18 +0900

    로또 번호 만들기 과제

--- src/ch08_collection/PlayList.java
+++ src/ch08_collection/PlayList.java
@@ -73,6 +73,7 @@ public class PlayList {
 			// :을 기준으로
 			String[] temp = arr.get(i).split(":");
 			// temp[0] 제목, temp[1] 가수
+			// 영문 대소문자 상관없이, 포함되어 있다면 검색되도록
 			if (option.equals("제목")) {
 				if (temp[0].contains(keyword)) {
 					result.add(arr.get(i));
--- src/practice/submit01/result07.java
+++ src/practice/submit01/result07.java
@@ -1,10 +1,49 @@
 package practice.submit01;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import ch08_collection.CollectionList;
+
 public class result07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Q 1. 로또번호 생성 메서드 1~45까지 랜덤 숫자 6개를 오름차순으로 출력하 는 makeLotto() 만들어주세요 (로또 번호는 중복이
+		 * 없습니다.)
+		 */
+		ArrayList<Integer> weekLotto = null;
+		weekLotto = makeLotto();
+		System.out.println(weekLotto.toString());
+		
+		/*
+		 * Q 2. 위의 로또번호 생성 메서드를 오버로딩하여 사용자의 희망 번호 최대 6 개를 매개변수로 전달받아 나머지 숫자를 1~45까지로 생성
+		 * 후 오름차순 으로 출력하는 makeLotto(String [] strArr) 만들어주세요 (로또 번호는 중복이 없습니다.) 참고사항:
+		 * 사용자의 매개변수 전달 값에도 중복은 없습니다.
+		 */
+		/*
+		 * Q 3. 위의 메서드를 활용하여 로또번호 생성 콘솔 프로그램을 만들어 주세요. [상황 1] n을 입력하면 전체 자동생성 실행 결과
+		 * 
+		 * [상황 2] y를 입력하고 4 10 과같이 띄어쓰기로 입력
+		 */
 
+	}
+	public static ArrayList<Integer> makeLotto() {
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		Random random = new Random();
+		for(int i = 0; i < 6; i++) {
+			
+			intList.add(random.nextInt(1,45));
+			for(int j = 0; j < intList.size(); j++) {
+				if(intList.get(i) == intList.get(j)) {
+					intList.remove(i);
+				}
+			}
+		}
+		Collections.sort(intList);
+		return intList;
+		
 	}
 
 }
