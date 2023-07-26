3060029968f5895b79f665c0ef056d34fc9dd711
Phantom <ksj0633@naver.com>
2023-07-26 17:45:38 +0900

    ..

--- src/practice/submit01/result07.java
+++ src/practice/submit01/result07.java
@@ -16,11 +16,17 @@ public class result07 {
 		weekLotto = makeLotto(args);
 		System.out.println(weekLotto.toString());
 		
+		
+		
+		
 		/*
 		 * Q 2. 위의 로또번호 생성 메서드를 오버로딩하여 사용자의 희망 번호 최대 6 개를 매개변수로 전달받아 나머지 숫자를 1~45까지로 생성
 		 * 후 오름차순 으로 출력하는 makeLotto(String [] strArr) 만들어주세요 (로또 번호는 중복이 없습니다.) 참고사항:
 		 * 사용자의 매개변수 전달 값에도 중복은 없습니다.
 		 */
+		String [] strArr = {"1", "3", "22"};
+		System.out.println(makeLotto(strArr));
+		
 		/*
 		 * Q 3. 위의 메서드를 활용하여 로또번호 생성 콘솔 프로그램을 만들어 주세요. [상황 1] n을 입력하면 전체 자동생성 실행 결과
 		 * 
@@ -32,6 +38,10 @@ public class result07 {
 		Random random = new Random();
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		// 랜덤한 값을 넣어준다.
+		for(int i = 0 ; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
 		while(intList.size() != 6) {
 			intList.add(random.nextInt(1,45));
 		
